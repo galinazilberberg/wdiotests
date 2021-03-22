cc94cc606716fdebe8bbf43bb668f8bd2eb90b44
Galina zilberberg <yinyangyogaloft@gmail.com>
2021-03-22 00:27:07 -0400

    commit sunday

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -4,7 +4,12 @@ class LoginPage extends Page {
     get inputUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password'); }
     get buttonSubmit () { return $('.login-form-button'); }
+    get errorToast() { return $('.ant-notification-notice-message');}
+    get fieldRequired() { return $('[role="alert"]')}
 
+    open () {
+        return super.open('/');
+    }
     setLogin (email){
         this.inputUsername.setValue(email);
     }
@@ -14,9 +19,19 @@ class LoginPage extends Page {
     clickSubmitButton(){
         this.buttonSubmit.click();
     }
-    open () {
-        return super.open('/');
+    submitButtonIsDisabled() {
+        expect(this.buttonSubmit).toBeDisabled();
     }
-}
-
+     errorToastAppeared(){
+            expect(this.errorToast).toBeDisplayed();
+        }
+    errorFieldIsRequired(){
+        expect(this.fieldRequired).toBeDisplayed();
+    }
+    errorFieldIsRequired(){
+        expect(this.fieldRequired).toBeDisplayed();
+    }
+    }
 export default new LoginPage();
+
+
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,14 +2,47 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page';
 
 describe('Auth', () => {
+    beforeEach(()=>{
+        LoginPage.open();//page open
+    });
+        afterEach( ()=> {
+            browser.execute ('window.localStorage.clear()');//clear cookies before every test
+        });
+        // browser.execute ('window.localStorage.clear()');//clear cookies before every test
+        // browser.refresh();//refresh no need because added afterEach clear cookies
+
     it('user logs in with valid data', () => {
-        LoginPage.open();
         LoginPage.setLogin('fifiti7257@gameqo.com') ;
         LoginPage.setPassword('GZ12345');
         LoginPage.clickSubmitButton();
-        ProfilePage.isOpen();
+        ProfilePage.isOpen();//refresh needed here otherwise test will look for button at user page
 
     });
+    it('submit button is disabled if log in and password are absent',()=>{
+        LoginPage.submitButtonIsDisabled();
+    });
+    it('auth fails if invalid date entered',()=>{
+        LoginPage.setLogin('example@example.com') ;
+        LoginPage.setPassword('12345');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+
+    });
+    it('auth fails if log in deleted',()=>{
+        LoginPage.setLogin('fifiti7257@gameqo.com') ;
+        LoginPage.setPassword('GZ12345');
+        LoginPage.setLogin();
+        LoginPage.clickSubmitButton();
+        LoginPage.errorFieldIsRequired();
+
+    });
+    it('auth fails if password deleted',()=>{
+        LoginPage.setLogin('fifiti7257@gameqo.com') ;
+        LoginPage.setPassword('GZ12345');
+        LoginPage.setPassword();
+        LoginPage.clickSubmitButton();
+        LoginPage.errorFieldIsRequired();
+
+    });
+
 });
-
-
